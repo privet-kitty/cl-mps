f1440913666153c45071b0b3e10ae51e637fd343
Hugo Sansaqua <privet.kitty99@gmail.com>
2022-05-03 02:42:40 +0900

    Improve test

--- test/main.lisp
+++ test/main.lisp
@@ -36,9 +36,32 @@ H ROW2
   (let ((problem
           (handler-bind ((warning (lambda (c) (declare (ignorable c)) (error "warn"))))
             (read-mps (make-string-input-stream
-                       "NAME Problem_Name
-OBJSENSE
-MAX
-")))))
+                       "NAME          Problem_Name
+ROWS
+ N  COST
+ L  LIM1
+ G  LIM2
+ E  MYEQN
+COLUMNS
+    XONE      COST                 1   LIM1                 1
+    XONE      LIM2                 1
+    YTWO      COST                 4   LIM1                 1
+    YTWO      MYEQN               -1
+    ZTHREE    COST                 9   LIM2                 1
+    ZTHREE    MYEQN                1
+RHS
+    RHS1      LIM1                 5   LIM2                10
+    RHS1      MYEQN                7
+BOUNDS
+ UP BND1      XONE                 4
+ LO BND1      YTWO                -1
+ UP BND1      YTWO                 1
+ENDATA")
+                      :default-sense +minimize+))))
     (is (string= (problem-name problem) "Problem_Name"))
-    (is (= (problem-sense problem) +maximize+))))
+    (is (string= (problem-objective-name problem) "COST"))
+    (is (= (problem-sense problem) +minimize+))
+    (symbol-macrolet ((constraints (problem-constraints problem)))
+      (is (= 3 (hash-table-count constraints)))
+      (dolist (row-name '("LIM1" "LIM2" "MYEQN"))
+        (is (gethash row-name constraints))))))
