246f1feb377881e611c0ad9f82d45cd6d9f56012
Hugo Sansaqua <privet.kitty99@gmail.com>
2022-05-03 00:44:20 +0900

    Implement

--- src/cl-mps.lisp
+++ src/cl-mps.lisp
@@ -1,34 +1,49 @@
 (defpackage :cl-mps
   (:use :cl)
   (:shadow #:variable)
-  (:export #:mps-syntax-warning #:mps-syntax-warning-line-number
-           #:sense #:variable #:problem #:+maximize+ #:+minimize+
-           #:read-mps))
+  (:export #:mps-syntax-warning #:mps-syntax-error #:mps-syntax-condition-line-number
+           #:sense #:variable #:+maximize+ #:+minimize+ #:read-mps
+           #:constraint #:constraint-name #:constraint-sense #:constraint-coefs #:constraint-rhs
+           #:problem #:make-problem #:problem-name #:problem-sense))
 
 (in-package :cl-mps)
 
-(define-condition mps-syntax-warning (simple-warning)
+(define-condition mps-syntax-condition (simple-condition)
   ((line-number :type (or null (integer 0))
                 :initarg :line-number
                 :initform nil
-                :reader mps-syntax-warning-line-number)))
+                :reader mps-syntax-condition-line-number)))
+(define-condition mps-syntax-warning (simple-warning mps-syntax-condition) ())
+(define-condition mps-syntax-error (simple-error mps-syntax-condition) ())
 
-(defmethod print-object ((warning mps-syntax-warning) stream)
+(defmethod print-object ((condition mps-syntax-condition) stream)
   (unless *print-escape*
-    (when (mps-syntax-warning-line-number warning)
-      (format stream "L~D: " (mps-syntax-warning-line-number warning))))
+    (when (mps-syntax-condition-line-number condition)
+      (format stream "at line ~D: " (mps-syntax-condition-line-number condition))))
   (call-next-method))
 
+(deftype sense () '(integer -1 1))
+
+;; objective senses
 (defconstant +maximize+ 1)
 (defconstant +minimize+ -1)
 
-(deftype sense () '(integer -1 1))
+;; constraint senses
+(defconstant +ge+ 1)
+(defconstant +eq+ 0)
+(defconstant +le+ -1)
 
 (defstruct variable
   (name nil :type string)
   (integer-p nil :type nil)
   (lo nil :type (or null real))
   (hi nil :type (or null real)))
+
+(defstruct constraint
+  (name nil :type string)
+  (sense nil :type sense)
+  (coefs nil :type hash-table)
+  (rhs nil :type real))
 
 (defstruct problem
   (name nil :type string)
@@ -40,25 +55,51 @@
   (delete-if-not (lambda (s) (> (length (the string s)) 0))
                  (the list (cl-ppcre:split "\\s+" string))))
 
-(defun read-mps (stream default-sense)
+(defun read-mps (stream &key (default-sense +minimize+))
   (check-type stream stream)
   (check-type default-sense sense)
-  (let ((problem (make-problem :name "" :sense default-sense)))
+  (let ((problem (make-problem :name "" :sense default-sense))
+        mode
+        (constraints (make-hash-table :test #'equal))
+        (non-constraints (make-hash-table :test #'equal)))
     (loop
       for line-number from 0
       for line = (read-line stream nil nil)
+      while line
+      for items = (%split line)
       do (block continue
-           (unless line (return))
-           (let ((items (%split line)))
-             (trivia:match (first items)
-               ("ENDATA" (return))
-               ("*" (return-from continue))
-               ("NAME"
-                (when (second items)
-                  (setf (problem-name problem) (second items)))
-                (when (third items)
-                  (warn 'mps-syntax-warning
-                        :line-number line-number
-                        :format-control "NAME contains whitespaces: ~S"
-                        :format-arguments (list line))))))))
+           (trivia:match (first items)
+             ("ENDATA" (return))
+             ("*" (return-from continue))
+             ("NAME"
+              (when (second items)
+                (setf (problem-name problem) (second items)))
+              (when (third items)
+                (warn 'mps-syntax-warning
+                      :line-number line-number
+                      :format-control "NAME contains whitespaces: ~A"
+                      :format-arguments (list line))))
+             ((or "ROWS" "COLUMNS" "RHS" "BOUNDS" "RANGES" "OBJSENSE")
+              (when (string= (first items) "RANGES")
+                (warn 'mps-syntax-warning
+                      :line-number line-number
+                      :format-control "RANGES section will be ignored"))
+              (setq mode (intern (first items) "CL-MPS")))
+             (otherwise
+              (ecase mode
+                (rows)
+                (columns)
+                (rhs)
+                (bounds)
+                (ranges)
+                (objsense
+                 (setf (problem-sense problem)
+                       (trivia:match (first items)
+                         ("MAX" +maximize+)
+                         ("MIN" +minimize+)
+                         (otherwise
+                          (error 'mps-syntax-error
+                                 :line-number line-number
+                                 :format-control "Unknown sense in OBJSENSE section: ~A"
+                                 :format-arguments (subseq items 0 1)))))))))))
     problem))
--- test/main.lisp
+++ test/main.lisp
@@ -9,3 +9,23 @@
 (def-suite main-suite)
 (in-suite main-suite)
 
+(test read-mps
+  (signals mps-syntax-warning
+    (read-mps (make-string-input-stream "NAME two words")))
+  (signals mps-syntax-warning
+    (read-mps (make-string-input-stream "RANGES")))
+  (signals mps-syntax-error
+    (read-mps (make-string-input-stream "OBJSENSE
+max?")))
+  (is (= (problem-sense (read-mps (make-string-input-stream "ENDATA"))) +minimize+))
+  (is (= (problem-sense (read-mps (make-string-input-stream "ENDATA")
+                                  :default-sense +maximize+))
+         +maximize+))
+  (let ((problem (read-mps (make-string-input-stream
+                            "NAME Problem_Name
+RANGES
+OBJSENSE
+MAX
+"))))
+    (is (string= (problem-name problem) "Problem_Name"))
+    (is (= (problem-sense problem) +maximize+))))
